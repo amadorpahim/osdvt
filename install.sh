d9b5a22da72f84f3a3f3761591d4db7e57c49d49
Amador Pahim <amador@pahim.org>
2012-10-05 16:46:47 -0300

    Installation improvements.
    Signed-off-by: Amador Pahim <amador@pahim.org>

--- install.sh
+++ install.sh
@@ -42,12 +42,18 @@ config_network()
     iface=${iface:-eth0}
     bridge=${bridge:-br0}
     [ $1 -eq 1 ] && {
-        echo $iface
-        cat /etc/sysconfig/network-scripts/ifcfg-$iface | grep -v -e DEVICE -e NAME -e UUID -e MAC -e TYPE -e ONBOOT > /etc/sysconfig/network-scripts/ifcfg-$bridge
-	echo -e "DEVICE=$bridge\nONBOOT=yes\nTYPE=Bridge\nNAME=$bridge" >> /etc/sysconfig/network-scripts/ifcfg-$bridge
-        echo -e "DEVICE=$iface\nONBOOT=yes\nBRIDGE=$bridge"  > /etc/sysconfig/network-scripts/ifcfg-$iface
+	[ -a /etc/sysconfig/network-scripts/ifcfg-$bridge ] && {
+		echo "File /etc/sysconfig/network-scripts/ifcfg-$bridge found. Skiping bridge creation."
+	} 
+	grep -i "BRIDGE=" /etc/sysconfig/network-scripts/ifcfg-$iface > /dev/null 2> /dev/null && {
+		echo "$iface is already configured to a bridge. Skipping bridge and interface configuration."
+	} || {
+       		cat /etc/sysconfig/network-scripts/ifcfg-$iface | grep -v -e DEVICE -e NAME -e UUID -e MAC -e TYPE -e ONBOOT > /etc/sysconfig/network-scripts/ifcfg-$bridge
+		echo -e "DEVICE=$bridge\nONBOOT=yes\nTYPE=Bridge\nNAME=$bridge" >> /etc/sysconfig/network-scripts/ifcfg-$bridge
+       		echo -e "DEVICE=$iface\nONBOOT=yes\nBRIDGE=$bridge"  > /etc/sysconfig/network-scripts/ifcfg-$iface
+       		service network restart
+	}
         chkconfig network on
-        service network restart
     }
 }
 
@@ -56,17 +62,19 @@ config_mysql()
     echo "OSDVT SERVER - MySQL Configuration"
     read -p "Please enter MySQL password: " mysqlpwd
     [ $1 -eq 1 ] && {
-        service mysqld start
+        service mysqld restart
         mysqladmin -u root password "${mysqlpwd}"
         chkconfig mysqld on
-        mysql --user=root --password="${mysqlpwd}" -e "create database db_osdvt"
+        mysql --user=root --password="${mysqlpwd}" -e "create database db_osdvt" 
     }
 }
 
 config_django()
 {
     [ $1 -eq 1 ] && {
-        cp /usr/local/osdvt/server/packaging/rhel63/django-settings.py /usr/local/osdvt/osdvtweb/settings.py
+	[ -a /usr/local/osdvt/osdvtweb/settings.py ] || {
+        	cp /usr/local/osdvt/server/packaging/rhel63/django-settings.py /usr/local/osdvt/osdvtweb/settings.py
+	}
 	sed -i "s/PASSWORD':\t''/PASSWORD':\t'${mysqlpwd}'/" /usr/local/osdvt/osdvtweb/settings.py
         cd /usr/local/osdvt/osdvtweb/
         python manage.py syncdb
@@ -104,12 +112,12 @@ main()
     distro=$?
 
     install_packets $distro
-    config_network $distro    
     config_mysql $distro    
     config_django $distro
     config_init $distro
     start_osdvtd $distro
     config_httpd $distro
+    config_network $distro    
     
 }
 
